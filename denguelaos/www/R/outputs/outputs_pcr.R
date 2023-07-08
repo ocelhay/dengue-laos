0cb6aff2581cbef8a5f1e77186777828e6c8f516
Olivier <olivier.celhay@gmail.com>
2023-07-08 11:44:18 -0700

    feat: filter `pcr_serortype_result`

--- denguelaos/www/R/outputs/outputs_pcr.R
+++ denguelaos/www/R/outputs/outputs_pcr.R
@@ -2,11 +2,11 @@
 output$table_patients_pcr_res <- renderTable({
   req(dengue_dta_filt())
   req(dengue_dta_filt() %>% nrow() >= 1)
-
+  
   dengue_dta_filt() %>%
     filter(pcr_result %in% c("Negative", "Equivocal", "Positive")) %>%
     pull(pcr_result) %>%
-  table_method_results()
+    table_method_results()
 })
 
 # Plot of patients, PCR results
@@ -47,9 +47,28 @@ output$plot_patients_pcr <- renderHighchart({
   req(dengue_dta_filt())
   req(dengue_dta_filt() %>% nrow() >= 1)
   
+  # List provided by Audrey DP on 2023-07-05.
+  display_cat <- c(
+    "DENV-1",
+    "DENV-2",
+    "DENV-3",
+    "DENV-4",
+    "DENV-1/2",
+    "DENV-1/3",
+    "DENV-1/4",
+    "DENV-2/3",
+    "DENV-2/4",
+    "DENV-3/4",
+    "No typing",
+    "Not done"
+  )
+  
   dengue_dta_filt() |>
-    filter(! is.na(collection_year), ! is.na(collection_month)) |> 
-    filter(! is.na(pcr_serortype_result), pcr_serortype_result != "Not done", pcr_serortype_result != "Unknown") |> 
+    filter(
+      ! is.na(collection_year), 
+      ! is.na(collection_month),
+      pcr_serortype_result %in% display_cat
+    ) |> 
     mutate(collection_year_month = as_date(glue("{collection_year}-{collection_month}-01")))  |>
     count(collection_year_month, pcr_serortype_result) |> 
     hchart(type = "column", hcaes(x = "collection_year_month", y = "n", group = "pcr_serortype_result")) |> 
